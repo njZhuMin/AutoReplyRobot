c649bd45a189fd79ab3dc1c003180d0b9d3fbdd4
SilverLining <minmin3772@gmail.com>
2016-08-09 17:40:00 +0800

    update auto-reply

---
+++ src/main/java/com/sunnywr/dao/CommandDao.java
@@ -0,0 +1,37 @@
+package com.sunnywr.dao;
+
+import com.sunnywr.bean.Command;
+import com.sunnywr.db.DBAccess;
+import org.apache.ibatis.session.SqlSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 与指令表对应的数据库操作类
+ */
+public class CommandDao {
+    /**
+     * 根据查询条件查询指令列表
+     */
+    public List<Command> queryCommandList(String name, String description) {
+        DBAccess dbAccess = new DBAccess();
+        List<Command> commandList = new ArrayList<Command>();
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = dbAccess.getSqlSession();
+            Command command = new Command();
+            command.setName(name);
+            command.setDescription(description);
+            // 通过sqlSession执行SQL语句
+            commandList = sqlSession.selectList("Command.queryCommandList", command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(sqlSession != null)
+                sqlSession.close();
+        }
+        return commandList;
+    }
+}
--- src/main/java/com/sunnywr/service/QueryService.java
+++ src/main/java/com/sunnywr/service/QueryService.java
@@ -1,10 +1,14 @@
 package com.sunnywr.service;
 
+import com.sunnywr.bean.Command;
+import com.sunnywr.bean.CommandContent;
 import com.sunnywr.bean.Message;
+import com.sunnywr.dao.CommandDao;
 import com.sunnywr.dao.MessageDao;
 import com.sunnywr.util.Iconst;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * 列表相关的业务
@@ -21,22 +25,24 @@ public class QueryService {
      * @return 自动回复的内容
      */
     public String queryByCommand(String command) {
-        MessageDao messageDao = new MessageDao();
-        List<Message> messageList;
+        CommandDao commandDao = new CommandDao();
+        List<Command> commandList;
         if(Iconst.HELP_COMMAND.equals(command)) {
-            messageList = messageDao.queryMessageList(null, null);
+            commandList = commandDao.queryCommandList(null, null);
             StringBuilder result = new StringBuilder();
-            for(int i = 0; i < messageList.size(); i++) {
+            for(int i = 0; i < commandList.size(); i++) {
                 if(i != 0) {
                     result.append("<br/>");
                 }
-                result.append("回复[" + messageList.get(i).getCommand() + "]可以查看" + messageList.get(i).getDescription());
+                result.append("回复[" + commandList.get(i).getName() + "]可以查看" + commandList.get(i).getDescription());
             }
             return result.toString();
         }
-        messageList = messageDao.queryMessageList(command, null);
-        if(messageList.size() > 0) {
-            return messageList.get(0).getContent();
+        commandList = commandDao.queryCommandList(command, null);
+        if(commandList.size() > 0) {
+            List<CommandContent> contentList = commandList.get(0).getContentList();
+            int i = new Random().nextInt(contentList.size());
+            return contentList.get(i).getContent();
         }
         return Iconst.NO_MATCHING_CONTENT;
     }
